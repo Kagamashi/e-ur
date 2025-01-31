1b490ccb2f13d23c23c6de4f83d0277e7c7e1076
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-31 16:52:27 +0100

    Modified: docs/compute/serverless/azure_container_instances.tf

--- docs/compute/serverless/azure_container_instances.tf
+++ docs/compute/serverless/azure_container_instances.tf
@@ -1,3 +1,36 @@
 # Azure Container Instace
 # serverless container deployment service that allows to run Docker containers without managing VMs or Kubernetes clusters
 
+# - deployment options - single container/multi-container groups
+
+resource "azurerm_container_group" "example" {
+  name                = "example-continst"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  container {
+    name   = "sidecar"
+    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
