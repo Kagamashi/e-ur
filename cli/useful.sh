19b3036b1b7d42b45d119c177ba1224f0c7bfac8
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-02 11:59:32 +0100

    Deleted: cli/useful.sh

--- cli/useful.sh
+++
@@ -1,5 +0,0 @@
-# Delete all ResourceGroups with 'product' in name
-for rg in $(az group list --query "[?contains(name, 'product')].name" -o tsv); do
-  az group delete --name $rg --yes --no-wait
-done
-
