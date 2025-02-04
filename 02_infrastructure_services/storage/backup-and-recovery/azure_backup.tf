5cea51f4382520dc96d1d23e539f00968f78ae7e
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-04 21:06:43 +0100

    Modified: 02_infrastructure_services/storage/backup-and-recovery/azure_backup.tf

--- 02_infrastructure_services/storage/backup-and-recovery/azure_backup.tf
+++ 02_infrastructure_services/storage/backup-and-recovery/azure_backup.tf
@@ -15,12 +15,27 @@
 # - security: built-in security for data in transit and at rest
 
 # Recovery Time Objective [RTO]: defines how quickly data mu be restored after a failure (max downtime allowed 4h = RTO eq 4h)
-# Recovery Point Objective [RPO]: 
+# Recovery Point Objective [RPO]: defines maximum allowable data loss measured in time (if RPO is 1h, backup must be taken every hour)
 
-# Types of replication:
-#   - LRS (Locally Redundant Storage): 3 copies within a single data center
-#   - ZRS (Zone Redundant Storage): 3 copie across different Availability Zones
-#   - GRS (Geo Redundant Storage): 6 copies (3 in primary region, 3 in secondary region)
+# Backup types:
+# - Full Backup - backs up entire dataset
+# - Incremental Backup - captures only changed data blocks since last backup
+# - Differential Backup - captures changes since the last full backup
+# - Transaction Log Backup - supports point-in-time recovery for databases
+
+# Backup security & availability:
+# - Storage Redundancy:
+#     LRS (Locally Redundant Storage): 3 copies within a single data center
+#     ZRS (Zone Redundant Storage): 3 copie across different Availability Zones
+#     GRS (Geo Redundant Storage): 6 copies (3 in primary region, 3 in secondary region)
+# - Data Encryption: uses RBAC and encryption
+# - Soft delete protection: deleted backups are retained for 14 days
+
+# Monitoring & compliance:
+# - Log Analytics Integration: tracks backup jobs and failures
+# - Backup Explorer: centralized dashboard for monitoring and troubleshooting
+# - Compliance Support: long-term retention policies for regulatory compliance
+
 
 resource "azurerm_recovery_services_vault" "example" {
   name                = "tfex-recovery-vault"
@@ -63,9 +78,3 @@ resource "azurerm_backup_policy_vm" "example" {
     months   = ["January"]
   }
 }
-
-# view backup jobs
-# az backup job list --resource-group rg-backup --vault-name backup-vault --query "[].{Status:status, VM:properties.entityFriendlyName, StartTime:startTime}" --output table
-
-# restore vm from backup
-# az backup restore restore-disks --resource-group rg-backup --vault-name backup-vault --container-name myvm --item-name myvm --target-resource-group my-rg --storage-account mysa --restore-type Original
