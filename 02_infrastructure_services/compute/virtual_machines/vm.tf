c44303076c555231e62c05e904c30a7bbed8cb8f
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-04 11:46:01 +0100

    Modified: 02_infrastructure_services/compute/virtual_machines/vm.tf

--- 02_infrastructure_services/compute/virtual_machines/vm.tf
+++ 02_infrastructure_services/compute/virtual_machines/vm.tf
@@ -19,52 +19,6 @@
 
 ###
 
-# Availability Sets [99.95% SLA]
-#   distributes VMs across fault domains (hardware racks)
-resource "azurerm_availability_set" "example" {
-  name                = "example-avset"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  platform_fault_domain_count = 3
-  platform_update_domain_count = 5
-}
-
-# Availability Zones [99.99% SLA]
-#   distributes VMs across different physical data centers in a region
-# resource "azurerm_virtual_machine" "example" {
-#   ...
-#   availability_zone   = "1"   # !!!
-# }
-
-###
-
-# Cost Optimization:
-#   - Reserved Instances (RI) - pre-pay for VMs for 1-3 years to save up to 72%
-#   good for predictable workloads
-resource "azurerm_capacity_reservation_group" "example" {
-  name                = "example-capacity-reservation-group"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-}
-
-resource "azurerm_capacity_reservation" "example" {
-  name                          = "example-capacity-reservation"
-  capacity_reservation_group_id = azurerm_capacity_reservation_group.example.id
-  sku {
-    name     = "Standard_D2s_v3"
-    capacity = 1 # amount of instances to be reserved
-  }
-}
-
-#   - Spot VMs - up to 90% lower cost
-#   good for batch jobs, CI/CD, fault-tolerant applications
-# resource "azurerm_virtual_machine" "example" {
-#   ...
-#   priority = spot   # !!!
-# }
-
-###
-
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.example.name
