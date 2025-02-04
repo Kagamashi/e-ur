6a94a3898854906ba38574eb9501e999aae0abee
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-04 14:27:23 +0100

    Modified: 02_infrastructure_services/networking/dns.tf

--- 02_infrastructure_services/networking/dns.tf
+++ 02_infrastructure_services/networking/dns.tf
@@ -1,16 +1,19 @@
-# DNS (Domain Name System) translates domain names
-  # A	: domain to an IPv4 address	: example.com → 192.168.1.1
-  # AAAA : domain to an IPv6 address : example.com → 2001:db8::1
-  # CNAME : alias one domain to another	: www.example.com → example.com
-  # MX : mail server settings	: example.com → mail.example.com
-  # TXT	: stores text data for verification	: SPF, DKIM, etc.
+# DNS (Domain Name System)
+# protocol in TCP/IP standard that translated human-readable domain names into IP addresses that computers use to communicate
+#   A	: domain to an IPv4 address	: example.com → 192.168.1.1
+#   AAAA : domain to an IPv6 address : example.com → 2001:db8::1
+#   CNAME : alias one domain to another	: www.example.com → example.com
+#   MX : mail server settings	: example.com → mail.example.com
+#   TXT	: stores text data for verification	: SPF, DKIM, etc.
+#   NS: specifies name servers for a domain
+#   SOA: identifies the primary name server
 
+###
 
 # Private DNS
-#   - resolve private domains inside Azure
-#   - NO internet required (internal for Azure)-
+#   - internal name resolution without external exposure
+#   - automatic hostname maintenance for VMs
 #   - used for private endpoints, VMs in VNETs
-
 resource "azurerm_private_dns_zone" "private_zone" {
   name                = "privatedb.local"
   resource_group_name = azurerm_resource_group.dns.name
@@ -34,10 +37,8 @@ resource "azurerm_private_dns_a_record" "db" {
 ###
 
 # Azure DNS
-#   - resolves public domain
-#   - internet required
-#   - used for websites, external-facing apps 
-
+# globally distributed DNS hosting service that allows domain management within the Azure ecosystem
+#   - alias record sets: directly point to Azure resources like lb, traffic manager, CDN endpoints
 resource "azurerm_dns_zone" "example" {
   name                = "example.com"
   resource_group_name = azurerm_resource_group.dns.name
