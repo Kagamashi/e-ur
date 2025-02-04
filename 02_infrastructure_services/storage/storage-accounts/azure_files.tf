a302e46a0b65cd2229bcb437dbdb3c27a5c61b3b
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-04 11:46:01 +0100

    Modified: 02_infrastructure_services/storage/storage-accounts/azure_files.tf

--- 02_infrastructure_services/storage/storage-accounts/azure_files.tf
+++ 02_infrastructure_services/storage/storage-accounts/azure_files.tf
@@ -1,19 +1,43 @@
 # Azure Files
-# fully managed file share service in Azure that provides SMB (Server Message Block) and NFS-based shared storage
-# it allows multiple VMs and services to access the same files simultaneously
-#   - NTFS ACLs for permission menagament
+# fully managed file shares in the cloud, accessible via SMB (Windows), NFS (Linux), or HTTP protocols
 
-# Protocols:
-#   - SMB 3.1.1: Windows/Linux VM network shares, hybrid access from on-prem
-#   - NFS 4.1: Linux VM workloads requiring NFS support
-#   - REST API: programmatic file storage for applications
-#   - Azure File Sync: sync on-prem Windows file shares with Azure
-
-# Types of replication:
+# Key characteristics:
+# • serverless (PaaS): no need to manage infrastracture
+# • almost unlimited storage: up to 100TiB per share, individual files up to 4TiB
+# • data encrypted at rest and in transit (HTTPS/SMB 3.0+)
+# • clients can mount from Windows, Linux, macOS
+# • Microsoft Entra integration
+# • snapshots and Azure Backup:
+#   - incremental snapshot: stores only changes, can restore individual files
+#   - soft delete for 1-365 days
+# • data redundancy:
 #   - LRS (Locally Redundant Storage): 3 copies within a single data center
 #   - ZRS (Zone Redundant Storage): 3 copie across different Availability Zones
 #   - GRS (Geo Redundant Storage): 6 copies (3 in primary region, 3 in secondary region)
-#   - RA-GRS (Read Access GRS): same as GRS but in secondary region only readable
+#   - GZRS (Geo Zone Redundant Storage): combines ZRS + GRS for best resillience
+
+# Authentication methods:
+#   • Identity-based (SMB): seamless SSO with AD DS identities
+#   • Access Key: full access to entire storage account (older, less secure)
+#   • SAS Token: restricted access for REST API calls
+
+# Use cases:
+#   • replace on-prem file servers with cloud-based
+#   • global file sharing across multiple OS and locations
+#   • lift-and-shift applications that expect a file system
+#   • Azure File Sync for on-prem caching and branch office scenarios
+
+# Azure File Sync:
+#   • caches Azure Files shares on Windows Server/cloud VM
+#   • supports SMB, NFS, FTPS for local access
+#   • cloud tiering: rarely used files are kept in Azure; frequently accessed files remain local
+#   • great for branch offices, DR (via Azure Backup), app lift-and-shift
+
+# Azure Storage Explorer:
+#    • cross-platform GUI for managing Azure Storage
+#    • supports connecting via Azure subs, SAS tokens, access keys
+#    • manage files, snapshots, directories
+
 
 resource "azurerm_storage_account" "example" {
   name                     = "azureteststorage"
